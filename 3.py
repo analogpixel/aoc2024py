47944f6ee08aa40eba6fe6ecaadf31d8e0dec5d9
poepping <matt.poepping@broadcom.com>
2025-10-24 10:37:33 -0600

    finished 3

--- 3.py
+++ 3.py
@@ -1,11 +1,36 @@
 #!/usr/bin/env python
 import re
 
-#data = open("data/3_demo.txt").read()
+#data = open("data/3_demo_2.txt").read()
 data = open("data/3.txt").read()
+
 count=0
 for m in re.findall( r"mul\(\d+,\d+\)", data):
     match = re.match( r"mul\((\d+),(\d+)\)", m)
     count += (int(match.group(1)) * int(match.group(2)))
 
-print(count)
+print("Part 1 count:", count)
+
+
+ldata = list(data)
+capture=True
+outstring=""
+
+for i in range(0, len(ldata) ):
+    if data[i:i+7] == "don't()":
+        capture=False
+    if data[i:i+4] == "do()":
+        capture=True
+
+    if capture:
+        outstring += data[i]
+
+
+
+count=0
+for m in re.findall( r"mul\(\d+,\d+\)", outstring):
+    match = re.match( r"mul\((\d+),(\d+)\)", m)
+    count += (int(match.group(1)) * int(match.group(2)))
+
+print("Part 2 count:", count)
+
