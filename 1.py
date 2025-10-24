465f758dc75ce36b896b5466d84db0e976139c00
poepping <matt.poepping@broadcom.com>
2025-10-24 10:40:23 -0600

    oops

--- 1.py
+++ 1.py
@@ -1,6 +1,23 @@
 #!/usr/bin/env python
+
 import re
 
-for f in open("data/1_demo.txt"):
-    m = re.match( r"(\d+)[ ]+(\d+)",f.strip())
-    print(m)
+x = []
+y = []
+
+for f in open("data/1.txt"):
+    m = re.match( r"(\d+)\s+(\d+)",f.strip())
+    x.append( int(m.group(1)))
+    y.append( int(m.group(2)))
+
+x = sorted(x)
+y = sorted(y)
+sum = 0
+sum2 = 0
+
+for i in range(0, len(x)):
+    sum +=  abs( x[i] - y[i] ) 
+    sum2 += x[i] * y.count(x[i])
+    
+print(sum)
+print(sum2)
